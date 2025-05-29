2a0d0a8f75d5141a23fc53f85eb2ad56ebf22512
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-05-30 00:05:30 +0200

    Creating the button

---
+++ alx-project-0x00/components/Button.tsx
@@ -0,0 +1,10 @@
+import {ButtonProp} from '@/interfaces/index';
+const Button: React.FC <ButtonProp> = ({title, styles}) => {
+    return (
+            <button className={`px-4 py-6 text-white bg-blue-500 ${styles}`}>
+                {title}
+            </button>
+    )
+}
+
+export default Button
--- alx-project-0x00/pages/landing.tsx
+++ alx-project-0x00/pages/landing.tsx
@@ -1,10 +1,12 @@
+import Button from "@/components/Button"
 import Card from "@/components/Card"
 
-const Landing: React.FC =  () => {
+const Landing: React.FC = () => {
     return (
       <div>
         <h1 className=" text-xl font-extralight">Landing Page</h1>
         <Card/>
+        <Button title="Small Button" styles="text-lg rounded-full"  />
       </div>
     )
   }
